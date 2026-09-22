5673d0de481d2ef120c3f1c7a5cbf9ef82fdcd2f
ANUGYA-byte <anugyaagrawal21@gmail.com>
2026-09-22 22:24:20 +0530

    Add files via upload

---
+++ Section 01 Basic Programs (1–15)/15.Area of Circle.py
@@ -0,0 +1,20 @@
+# ---------------------------------------
+# Program 15: Area of Circle
+# Description: Calculates the area of a circle using the given radius.
+# Author: Anugya Agrawal
+# ---------------------------------------
+import math
+
+radius = float(input("Enter Radius: "))
+area_of_circle = math.pi * radius ** 2
+
+print("Area of circle:", round(area_of_circle, 2))
+
+# ---------------------------------------
+# SAMPLE INPUT
+# Enter Radius: 12
+# ---------------------------------------
+
+# SAMPLE OUTPUT
+# Area of circle: 452.39
+# ---------------------------------------
