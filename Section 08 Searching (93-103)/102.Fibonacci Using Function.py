6b75c4ea5cccfe3a8ded58bce6d81c85035803fc
ANUGYA-byte <304574911+ANUGYA-byte@users.noreply.github.com>
2026-09-24 10:29:08 +0530

    add sec 8

---
+++ Section 08 Searching (93-103)/102.Fibonacci Using Function.py
@@ -0,0 +1,13 @@
+# ---------------------------------------
+# Program 96: Fibonacci Using Function
+# Description: Prints Fibonacci series using a function.
+# Author: Anugya Agrawal
+
+def fibonacci(n):
+    a, b = 0, 1
+    for i in range(n):
+        print(a, end=" ")
+        a, b = b, a + b
+
+n = int(input("Enter number of terms: "))
+fibonacci(n)
