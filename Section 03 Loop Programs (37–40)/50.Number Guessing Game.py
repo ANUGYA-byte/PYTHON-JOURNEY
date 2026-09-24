2240871eb7ed079a7a8df884c01eb299d8852e47
ANUGYA-byte <304574911+ANUGYA-byte@users.noreply.github.com>
2026-09-24 11:45:14 +0530

    update

---
+++ Section 03 Loop Programs (37–40)/50.Number Guessing Game.py
@@ -0,0 +1,19 @@
+# ---------------------------------------
+# Program 80: Number Guessing Game
+# Description: Allows the user to guess a randomly generated number.
+# Author: Anugya Agrawal
+
+import random
+
+number = random.randint(1, 100)
+
+while True:
+    guess = int(input("Guess the number (1-100): "))
+
+    if guess == number:
+        print("Congratulations! You guessed it correctly.")
+        break
+    elif guess < number:
+        print("Too low! Try again.")
+    else:
+        print("Too high! Try again.")
